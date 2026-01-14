ac1feabf0cf29e862937b999c55a85300eca7820
smsns6089-bit <smsns6089@gmail.com>
2026-01-14 16:34:41 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1960,7 +1960,7 @@ function drawWeaponViewmodel(w, dt) {
   const aimAng = Math.atan2(aimY - y, aimX - x);
 
   // Gun art is drawn extending +X, so rotate to point at crosshair
-  ctx.rotate(aimAng + game.recoil * 0.04);
+ ctx.rotate(aimAng + Math.PI + game.recoil * 0.04);
 
   // Pivot so it sits on-screen and doesn't "orbit"
   // (ONLY ONE pivot translate. Too many = gun disappears)
